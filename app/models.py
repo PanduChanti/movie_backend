005833b1716aa9e02aa7d9783db9346f9de4c7b6
panduchanti <akuratipandurangarao@gmail.com>
2023-04-17 15:15:23 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from datetime import datetime
+
+
+# Create your models here.
+
+class MovieDataBase(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="images/")
+    movie = models.FileField(upload_to="movies/")
+    desc = models.CharField(max_length=255)
+    createdat = models.DateTimeField(default=datetime.now)
+    language = models.CharField(max_length=255)
+    genres = models.CharField(max_length=255)
+    quality = models.CharField(max_length=255)
